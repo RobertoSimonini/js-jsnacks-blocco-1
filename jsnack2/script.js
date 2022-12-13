a87198e4345856dc0524ea3795dfce842a87fe2c
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-13 15:26:29 +0100

    got elements from the DOM

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,14 @@
 //// console.log ('Prova Jsnack 2');
+
+/* 
+    !SNACK 2 
+    In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l’utente inserisce il suo nome tramite un input.
+    comunicagli se può partecipare o no alla festa.
+*/
+
+// preparo l'array con i nomi dei partecipanti invitati 
+const allowedPeople = ['Pippo', 'Roberto', 'Claudio', 'Ciccio'];
+
+// Prendo dal Dom l'input e il bottone 
+const nameElement = document.getElementById('username');
+const getInBtn = document.getElementById('get-in');
