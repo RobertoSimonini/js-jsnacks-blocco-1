94d6e2bbc62a8fdc0178453c67fed120f9b7bedd
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-13 16:43:01 +0100

    Snack 3 completed

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -10,19 +10,27 @@
 // Prendo l'imput e il bottone per eseguire la somma dal DOM
 const userNumber = document.getElementById('userNumber');
 const btnSum = document.getElementById('btn-sum');
+const message = document.getElementById('message');
 
-// Aggiungo l'event listener al bottone per eseguire la somma 
+
+// Aggiungo l'event listener al bottone per eseguire la somma
+
 btnSum.addEventListener ('click', function(){
-
 // Prendo il value dell'input 
- const userNumberValue = userNumber.value;
-
+const userNumberValue = userNumber.value;
+    
 // ! Verifica 
-
 if ((!userNumberValue) || isNaN(userNumberValue)) {
     alert('Inserisci valori validi');
     return;
-}
+};
+    
+// Assegno una variabile per sommare le unità
+let sumDigits = 0;
+for  (let i = 0; i <userNumberValue.length; i++) {
+    sumDigits += parseInt(userNumberValue[i]);
+};
 
-
+// Stampo in pagina il risultato 
+message.innerText = 'La somme delle quattro unità è: ' + sumDigits;
 });
